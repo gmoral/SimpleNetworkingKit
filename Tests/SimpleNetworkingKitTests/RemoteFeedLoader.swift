61012d3601615cb19559d1f6f11bd432a9cbe2c5
Guillermo Moral <willy.moral@gmail.com>
2023-01-06 16:07:25 -0300

    Request data from URL on load

--- Tests/SimpleNetworkingKitTests/RemoteFeedLoader.swift
+++ Tests/SimpleNetworkingKitTests/RemoteFeedLoader.swift
@@ -4,25 +4,40 @@
 //
 //  Created by Guillermo Moral on 06/01/2023.
 //
+// Property Injection
+// Method Injection
+// Constructor Injection
 
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
     
 }
