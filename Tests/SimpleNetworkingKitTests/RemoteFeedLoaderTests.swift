70ed41fca19e2990be30dae2f0253338eaa68175
Guillermo Moral <willy.moral@gmail.com>
2023-01-07 10:06:32 -0300

    Delivers connectivity error on client error

--- Tests/SimpleNetworkingKitTests/RemoteFeedLoaderTests.swift
+++ Tests/SimpleNetworkingKitTests/RemoteFeedLoaderTests.swift
@@ -11,14 +11,6 @@
 import XCTest
 @testable import SimpleNetworkingKit
 
-class HTTPClientSpy : HTTPClient {
-    var requestedURLs = [URL]()
-    
-    func get(from url: URL) {
-        requestedURLs.append(url)
-    }
-}
-
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
@@ -46,6 +38,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        
+        sut.load { error in capturedError = error }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         
@@ -53,5 +57,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let sut = RemoteFeedLoader(url: url, client: client)
         
         return (sut, client)
+    }
+    
+    private class HTTPClientSpy : HTTPClient {
+        var requestedURLs = [URL]()
+        var error : Error?
+        
+        func get(from url: URL, completion: @escaping(Error) -> Void) {
+            
+            if let error = error {
+                completion(error)
+            }
+            
+            requestedURLs.append(url)
+        }
     }
 }
